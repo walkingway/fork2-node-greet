70cdd803c443cfba580ff1f1adcce2532c9958bc
walkingway <walkingway@126.com>
2014-04-29 20:52:43 +0800

    Writing The First Test

---
+++ test/greet_spec.js
@@ -0,0 +1,14 @@
+var expect = require("chai").expect
+var greet = require("greet")
+
+describe('greet', function(){
+	it("should greet a person by name", function(){
+		expect(greet("cheng")).to.eql("hello, cheng");
+	});
+});
+
+describe('greet', function(){
+	it("should greet a person flirtatiously if drunk", function(){
+		expect(greet("way","drunk")).to.eql("hello way, you look sexy today");
+	});
+});
