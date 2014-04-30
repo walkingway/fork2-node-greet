f892b4e23d8e7e1d04813c86015b95c6bda3461e
walkingway <walkingway@126.com>
2014-04-30 23:15:43 +0800

    Convert bin/greet.js

--- bin/greet.js
+++ bin/greet.js
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
-greet = require("../lib/index.js");
-var argv = require('minimist')(process.argv.slice(2));
-var name = argv._[0];
-var drunk = argv["drunk"];
-console.log(greet(name,drunk));
+// greet = require("../lib/index.js");
+// var argv = require('minimist')(process.argv.slice(2));
+// var name = argv._[0];
+// var drunk = argv["drunk"];
+// console.log(greet(name,drunk));
+
+var command = require("../lib/command");
+command();
